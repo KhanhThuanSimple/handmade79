25e095deddba1f196a0853e09383f14a310ca2c4
KhanhThuanSimple <22130277@st.hcmuaf.edu.vn>
2026-01-15 21:13:05 +0700

    update 111111112

--- HandMade/scripts/server.js
+++ HandMade/scripts/server.js
@@ -1,14 +1,14 @@
 const jsonServer = require('json-server');
 const path = require('path');
-const server = jsonServer.create();
 
-// Sử dụng path.join và __dirname để đảm bảo luôn tìm thấy file db.json 
-// dù deploy ở bất cứ đâu trên server Vercel
-const dbPath = path.join(__dirname, 'db.json');
-const router = jsonServer.router(dbPath);
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(router);
+server.use(jsonServer.bodyParser);
+
+// KHÔNG dùng /api
+server.use('/', router);
 
 module.exports = server;
